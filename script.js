959797ee1758fae60e5097a79171cd2ce560c950
Snikie1 <pavlenkofficial.co@gmail.com>
2024-06-15 10:03:45 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+window.Telegram.WebApp.ready();
+
+const initData = window.Telegram.WebApp.initData || '';
+const initDataUnsafe = window.Telegram.WebApp.initDataUnsafe || {};
+
+console.log(initData, initDataUnsafe);
